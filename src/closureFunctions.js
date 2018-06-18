293b2610fd289ba0e025f0f6fb8d9e6d7f5bc113
Deepali Garg <hdgarg@gmail.com>
2018-06-17 22:43:41 -0700

    completes closure assignment with all tests passing successfully

--- src/closureFunctions.js
+++ src/closureFunctions.js
@@ -3,15 +3,24 @@
 // a complete greeting in the form of "{greeting}, {person}"
 
 // EXAMPLE
-// var helloGreeting = greeting('Hello');
+// var helloGreeting = greetingFactory('Hello');
 // helloGreeting('Sam'); // 'Hello, Sam'
-// var holaGreeting = greeting('Hola');
+// var holaGreeting = greetingFactory('Hola');
 // holaGreeting('Shi-Hao'); // 'Hola, Shi-Hao'
+ 
+//works
+// var greetingFactory = function (greeting) {
+//   // INSERT CODE HERE  
+//   return function(name) {
+//     return greeting + ', ' + name;
+//   };
+// };
+
 
 var greetingFactory = function(greeting) {
-  // INSERT CODE HERE
-}
-
+  // INSERT CODE HERE  
+  return name =>`${greeting}, ${name}`;
+};
 // sumCalculator takes in an initial number that is stored and returns a function
 // that continues adding to the stored value. The function returned by
 // sumCalculator should take as many numbers as passed in, sum them to the
@@ -27,8 +36,16 @@ var greetingFactory = function(greeting) {
 // calculatorTwo() // 10
 
 var sumCalculator = function(initialValue) {
-  // INSERT CODE HERE
-}
+  return function() {
+    if (!initialValue) {
+      return 0;
+    }
+    for (var i = 0; i < arguments.length; i++) {
+      initialValue += arguments[i];
+    }
+    return initialValue;
+  }
+};
 
 // coinFlipTally should generate a storage object, take no arguments, and 
 // return another object with two methods, makeCoinFlip and checkTally. The returned 
@@ -57,9 +74,32 @@ var sumCalculator = function(initialValue) {
 // gameTally.checkTally(); // should have updated count with heads added or tails added
 
 var coinFlipTally = function() {
-  // INSERT CODE HERE
+  var tally = { heads:0, tails:0};
+  //variable object
+  makeCoinFlip = () => {
+    //initialize variable to record random number 0 or 1;
+    var coinFlipResult = Math.floor(Math.random() * 2) ? 'heads' : 'tails';
+    tally[coinFlipResult]++;
+    return coinFlipResult;      
+  } 
+  checkTally = () => {
+    return tally;     
+  };
+  return {
+    makeCoinFlip, checkTally
+  };
+};
 
-}
+var gameTally = coinFlipTally();
+gameTally.makeCoinFlip(); // returns 'heads' or 'tails'
+console.log(gameTally.checkTally()); // should have updated count with heads added or tails added
+gameTally.makeCoinFlip();
+console.log(gameTally.checkTally()); // should have updated count with heads added or tails added
+gameTally.makeCoinFlip();
+console.log(gameTally.checkTally()); // should have updated count with heads added or tails added
+gameTally.makeCoinFlip();
+console.log(gameTally.checkTally()); 
+
 
 module.exports = {
   greetingFactory: greetingFactory,
